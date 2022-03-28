9b3b2ba81b161fbd2837bab6eafbd3ec937d9183
dialupmodem <dialup.modem113@gmail.com>
2022-03-28 16:13:57 -0400

    implement bootstrap collapse in favor of existing solution

--- src/components/CategoryNav.vue
+++ src/components/CategoryNav.vue
@@ -23,27 +23,67 @@
                   class="nav-link pe-0 ps-0 m-0"
                   :class="{ active: isActiveCategory(weaponCategory.id) }"
                   @click="changeCategory(weaponCategory.id)"
-                >{{ weaponCategory.name }}</button>
+                >
+                  {{
+                    weaponCategory.name
+                  }}
+                </button>
               </div>
               <div class="col-2 align-self-center justify-self-end">
                 <button
                   type="button"
-                  class="category-accordion pt-1"
+                  class="category-accordion"
+                  data-bs-toggle="collapse"
+                  :data-bs-target="`#categoryCollapse-${weaponCategory.id}`"
+                  :style="getAccordionButtonStyle(weaponCategory.id)"
                   @click="toggleCategoryAccordion(weaponCategory.id)"
                 >
                   <i :class="getAccordionIconClass(weaponCategory.id)"></i>
                 </button>
               </div>
             </div>
-            <div class="row" v-if="isCategoryAccordionExpanded(weaponCategory.id)">
+            <div class="row">
               <div class="col">
-                <ul class="nav flex-column flex-nowrap">
-                  <li class="nav-item" :key="weapon.id" v-for="weapon in weaponCategory.weapons">
-                    <a class="nav-link" href="#" @click="changeWeapon(weapon.id)">{{ weapon.name }}</a>
-                  </li>
-                </ul>
+                <div class="collapse" :id="`categoryCollapse-${weaponCategory.id}`">
+                  <ul class="nav flex-column flex-nowrap">
+                    <li class="nav-item" :key="weapon.id" v-for="weapon in weaponCategory.weapons">
+                      <a
+                        class="nav-link"
+                        href="#"
+                        @click="changeWeapon(weapon.id)"
+                      >{{ weapon.name }}</a>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
+            <!-- <div class="row">
+              <div class="col">
+                <div class="accordion-container">
+                  <transition name="expand">
+                    <div
+                      class="accordion-content"
+                      :key="`${weaponCategory.id}-accordion`"
+                      v-if="isCategoryAccordionExpanded(weaponCategory.id)"
+                    >
+                      <ul class="nav flex-column flex-nowrap">
+                        <li
+                          class="nav-item"
+                          :key="weapon.id"
+                          v-for="weapon in weaponCategory.weapons"
+                        >
+                          <a
+                            class="nav-link"
+                            href="#"
+                            @click="changeWeapon(weapon.id)"
+                          >{{ weapon.name }}</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </transition>
+                </div>
+              </div>
+            </div>-->
           </div>
         </li>
       </ul>
@@ -86,8 +126,27 @@ export default {
         return
       }
 
-      let iconClass = categoryAccordion.expanded ? 'bi-chevron-down' : 'bi-chevron-right'
+      //let iconClass = categoryAccordion.expanded ? 'bi-chevron-down' : 'bi-chevron-right'
+      let iconClass = 'bi-chevron-right'
       return iconClass
+    },
+    getAccordionButtonStyle(categoryId) {
+      let isCategoryExpanded = this.isCategoryAccordionExpanded(categoryId)
+
+      let styleObject = {
+        transform: isCategoryExpanded ? "rotate(90deg)" : "rotate(0deg)"
+      }
+
+      return styleObject
+    },
+    getAccordionContentStyle(categoryId) {
+      let category = this.weaponCategories.find(c => c.id == categoryId)
+
+      let styleObject = {
+        maxHeight: `${40 * category.weapons.length}px`
+      }
+
+      return styleObject
     },
     isCategoryAccordionExpanded(categoryId) {
       let categoryAccordion = this.categoryAccordions.find(c => c.id == categoryId)
@@ -132,6 +191,7 @@ export default {
   background-color: transparent;
   border: none;
   color: #fffafa;
+  transition: transform 0.5s;
 }
 h4,
 hr {
@@ -163,9 +223,26 @@ hr {
     color: #909090;
   }
 }
-.collapse {
-  @media screen and (min-width: map-get($grid-breakpoints, lg)) {
-    display: block !important;
-  }
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: max-height 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  max-height: 100%;
+}
+.accordion-container {
+  overflow: hidden;
+}
+.accordion-content {
+}
+.expand-enter-active,
+.expand-leave-active {
+  transition: height 0.5s;
+}
+.expand-enter,
+.expand-leave-to {
+  max-height: 0;
 }
 </style>
